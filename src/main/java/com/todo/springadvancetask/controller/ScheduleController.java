94354bac7d7b0ec95766d7c3cdfac171a5db07ba
Woojin1123 <boom175839@gmail.com>
2024-08-23 16:28:11 +0900

    일정 삭제 추가

--- src/main/java/com/todo/springadvancetask/controller/ScheduleController.java
+++ src/main/java/com/todo/springadvancetask/controller/ScheduleController.java
@@ -3,14 +3,17 @@ package com.todo.springadvancetask.controller;
 import com.todo.springadvancetask.dto.schedule.ScheduleRequestDto;
 import com.todo.springadvancetask.dto.schedule.ScheduleResponseDto;
 import com.todo.springadvancetask.service.ScheduleService;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,8 +33,12 @@ public class ScheduleController {
   }
 
   @GetMapping // 전체조회
-  public ResponseEntity getAllSchedules() {
-    return null;
+  public ResponseEntity getAllSchedules(
+      @RequestParam(name = "page_no", defaultValue = "0") int pageNo,
+      @RequestParam(name = "page_size", defaultValue = "10") int pageSize) {
+    List<ScheduleResponseDto> responseDtos = scheduleService.getAllSchedules(pageNo, pageSize);
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(responseDtos);
   }
 
   @PostMapping
@@ -42,9 +49,17 @@ public class ScheduleController {
   }
 
   @PutMapping("/{id}")
-  public ResponseEntity updateSchedule(@PathVariable Long id,@RequestBody ScheduleRequestDto requestDto) {
+  public ResponseEntity updateSchedule(@PathVariable Long id,
+      @RequestBody ScheduleRequestDto requestDto) {
     ScheduleResponseDto responseDto = scheduleService.updateSchedule(id, requestDto);
     return ResponseEntity.status(HttpStatus.OK)
         .body(responseDto);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity deleteSchedule(@PathVariable Long id) {
+    Long deletedId = scheduleService.deleteSchedule(id);
+    return ResponseEntity.status(HttpStatus.ACCEPTED)
+        .body(deletedId);
   }
 }
